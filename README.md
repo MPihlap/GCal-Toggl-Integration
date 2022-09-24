01ec4e1f0c91a177b6cd68a19b4d81065325376e
Meelis Pihlap <meelispihlap@gmail.com>
2022-09-24 23:30:56 +0300

    Updated readme

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Integration to create time entries automatically based on google calendar events
 ## Usage
 
 1. Setup the google calendar API (https://developers.google.com/calendar/api/quickstart/python)
-2. Install requirements using `python3 -m pip install -r requirements.txt`
-3. Modify `config.yaml` to customize behaviour to your needs.
-4. Run the `gcal_toggl_integration.py` file to add events from the past 24 hours or create your own scripts based on the example found in the `main` function.
+    * Use the OAuth2 desktop app credentials
+2. Save your toggl API token into a file named "toggl_token.txt".
+3. Install requirements using `python3 -m pip install -r requirements.txt`
+4. Modify `config.yaml` to customize behaviour to your needs.
+5. Run the `gcal_toggl_integration.py` file to add events from the past 24 hours or create your own scripts based on the example found in the `main` function.
--- gcal_toggl_integration.py
+++ gcal_toggl_integration.py
@@ -12,35 +12,7 @@ from googleapiclient.discovery import build
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-from toggl import api
-from toggl.utils import Config
-
-class TogglInterface:
-  def __init__(self, token_filename="toggl_token.txt") -> None:
-    token = ""
-    with open(token_filename) as f:
-        token = f.read().strip()
-
-    self.config = Config.factory()  # Without None it will load the default config file
-    self.config.api_token = token
-    self.config.timezone = 'utc'  # Custom timezone
-
-  def add_entry(self, description=None, start_time=None, stop_time=None, project_id=None):
-    entry = api.TimeEntry(start_time, stop_time, description=description, project=project_id)
-    entry.save()
-
-  def start_tracking(self, description, project_id=None, client=None):
-    api.TimeEntry.start_and_save(
-        config=self.config,
-        description=description,
-        project=project_id
-    )
-
-  def stop_tracking(self):
-    current = api.TimeEntry.objects.current(config=self.config)
-    if current is not None:
-      current.stop_and_save()
-
+from toggl_interface import TogglInterface
 
 class GcalInterface():
 
